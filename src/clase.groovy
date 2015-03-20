f3ca5df9a2e12ca692265d63c6b6cc5b8c848f9a
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2015-03-19 21:58:48 -0500

    Ejercicio de each

--- src/clase.groovy
+++ src/clase.groovy
@@ -1,16 +1,19 @@
 def pali = {list -> list.any{x -> x.toLowerCase().replaceAll(" ","") ==  x.toLowerCase().replaceAll(" ","").reverse()}}
-/*
+
 println pali(["Añora la Roña",
               "La ruta natural","Acaso hubo buhos aca",
-              "Se van sus naves"])
-
-*/
+              "Se van sus nave"])
 
 def tele = {n -> (1..n).collect{ (1..it).collect { x -> (1..x).collect{print x}}}}
+s =  tele(4)
 
-    s =  tele(4)
+println()
+
+def clousu = {x,list,res =[] -> list.each{print(it.call(x)+" ")}}
 
 
-def clousu = {x,list -> list.each{it.call(x)}}
+def sum = {x-> x+2}
+def res = {x-> x-2}
+clousu(1,[sum,res])
 
-clousu(["HOLA"],[pali])
+
